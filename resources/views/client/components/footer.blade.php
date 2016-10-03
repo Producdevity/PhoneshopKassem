5e55b9c125efb4e224241acc256838d8d4488482
yassine kassem <yassine kassem>
2016-10-03 19:29:09 +0200

    first commit, laravel init

---
+++ resources/views/client/components/footer.blade.php
@@ -0,0 +1,47 @@
+<div class="footer">
+    <div class="container">
+        <div class="footer-top-at">
+            <div class="col-md-4 amet-sed">
+                <h4>PHONESHOP KASSEM</h4>
+                <p>Gecertificeerde monteurs</p>
+                <p>Klaar terwijl u wacht</p>
+                <p>Hoogste kwaliteit onderdelen</p>
+                <p>No cure no pay</p>
+                <p>3 maanden garantie</p>
+            </div>
+            <div class="col-md-4 amet-sed ">
+                <h4>CONTACT</h4>
+
+                <p>BTW:
+                    <?php echo $btwNr; ?> </p>
+                <p>Kvk:
+                    <?php echo $kvkNr; ?>
+                </p>
+                <p>Tel:
+                    <?php echo $telNr; ?>
+                </p>
+                <p>Mail:
+                    <?php echo $mail; ?>
+                </p>
+                <ul class="social">
+                    <li><a href="<?php echo $fbLink; ?>"><i class="facebook"> </i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 amet-sed ">
+                <h4>Openingstijden</h4>
+                <p>Ma 12:00 - 21:00</p>
+                <p>Di 10:00 - 21:00</p>
+                <p>Wo 10:00 - 21:00</p>
+                <p>Do 10:00 - 21:00</p>
+                <p>Vr 10:00 - 21:00</p>
+                <p>Za 12:00 - 18:00</p>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+    <div class="footer-class">
+        <p>© 2016 Phoneshop Kassem | All Rights Reserved | <a href="<?php echo $devLink; ?>" target="">Yassine Gherbi</a> </p>
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/client/components/header.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Phoneshop Kassem</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Custom Theme files -->
+
+    <!--theme-style-->
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <!--//theme-style-->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="telefoon reparatie alphen, reparatie goedkoop, reparatie alphen aan den rijn, telefoonshop kassem, reparatiewinkel kassem, phoneshop, kassem, reparatie, tablet, telefoon, Alphen aan den Rijn, Phoneshopkassem, phone and tablet repair"
+    />
+    <script type="application/x-javascript">
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+
+    <!--fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+    <!--//fonts-->
+
+    <!-- start menu -->
+    <link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+    <script type="text/javascript" src="js/memenu.js"></script>
+    <script>
+        $(document).ready(function() {
+            $(".memenu").memenu();
+        });
+    </script>
+    <script src="js/simpleCart.min.js"></script>
+</head>
+
+<body>
+<!--header-->
+<div class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header-left">
+                <ul>
+                    <li><a href="tel:<?php echo $telNr; ?>">Telefoon: <?php echo $telNr; ?></a></li>
+                    <li><a>Krijgt u niemand aan de lijn? Wij bellen u altijd terug</a></li>
+                    <li><a href="mailto: <?php echo $mail; ?>">Mail: <?php echo $mail; ?></a></li>
+                    <li><a>3 maanden garantie</a></li>
+                </ul>
+                <div class="cart box_1">
+                    <a href="<?php echo $fbLink; ?>">
+                        <h3> <div class="total"></div>
+                          <img src="images/fb-icon.png" alt=""/>
+                        </h3>
+                    </a>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="head-top">
+            <div class="logo">
+              <a href="?url"><img src="images/logos/phoneshop-kassem-logo.png" alt=""></a>
+            </div>
+            <div class=" h_menu4">
+                <ul class="memenu skyblue">
+                    <li class="active grid"><a class="color8" href="?url=">Home</a></li>
+                    <li><a class="color2" href="?url=mobileBrands">Telefoon</a>
+                        <div class="mepanel">
+                            <div class="row">
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Telefoons</h4>
+                                        <ul>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <ul>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Merken</h4>
+                                        <ul>
+                                          <li><a href="?url=mobile&brand_id=brandid">brandname</a></li>
+                                          <li><a href="?url=mobile&brand_id=brandid">brandname</a></li>
+                                          <li><a href="?url=mobile&brand_id=brandid">brandname</a></li>
+                                          <li><a href="?url=mobile&brand_id=brandid">brandname</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="grid"><a class="color2" href="?url=tabletBrands">Tablet</a>
+                        <div class="mepanel">
+                            <div class="row">
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Tablets</h4>
+                                        <ul>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                          <li><a href="?url=details&device_id='.$device['id'].'">' . $fullName . '</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <ul>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                          <li><a href="?url=details&device_id=deviceid">fullname</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Merken</h4>
+                                        <ul>
+                                          <li><a href="?url=tablet&brand_id=">brandname</a></li>
+                                          <li><a href="?url=tablet&brand_id=">brandname</a></li>
+                                          <li><a href="?url=tablet&brand_id=">brandname</a></li>
+                                          <li><a href="?url=tablet&brand_id=">brandname</a></li>
+                                          <li><a href="?url=tablet&brand_id=">brandname</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a class="color1" href="?url=info">Informatie</a></li>
+                    <li><a class="color6" href="?url=contact">Contact</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div><!-- /.container -->
+</div><!--/.header-->
